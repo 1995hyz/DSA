dcc50feef3dbecad992b45db2435dd6db2f3d6df
1995hyz <1995hyz@gmail.com>
2018-03-24 22:12:01 -0400

    Add files via upload

--- listSim.cpp
+++ listSim.cpp
@@ -5,6 +5,7 @@
 #include <list>
 #include <errno.h>
 #include <fstream>
+#include <sstream>
 #include <string.h>
 
 using namespace std;
@@ -12,7 +13,7 @@ using namespace std;
 template <typename Object>
 class simpleList
 {
-	protected:
+	private:
 		class Node {
 			public:	
 				Node *nextNode;
@@ -37,6 +38,12 @@ class simpleList
 		void setTheSize(int size){
 			theSize=size;
 		}
+		virtual void push(Object i){
+			cout<<"Not Supposed to Run!"<<endl;
+		}
+		virtual Object pop(){
+			cout<<"Not Supposed to Run!"<<endl;
+		}
 	protected:
 		void create(Object i){
 			Node* firstNode=new Node(i,NULL);
@@ -59,20 +66,14 @@ class simpleList
 			if(theSize>0){
 				Node *temp=head;
 				head=head->nextNode;
-				cerr<<"Value popped: "<<temp->dataField;
 				delete temp;
 				theSize--;
-				cerr<<" Size: "<<theSize<<endl;
-			}
-			else{	
-				cerr<<"ERROR: This list is empty!"<<endl;
 			}
 		}
-		virtual void push(Object i){
+		Object getHeadData(){
+			return head->dataField;
 		}
-		virtual Object pop(){
-		}
-	protected:
+	private:
 		int theSize;
 		Node *head;
 		Node *tail;
@@ -84,19 +85,25 @@ class stack: public simpleList<Object>
 {
 	public:
 		stack(){
-			simpleList<Object>::theSize=0;
+			simpleList<Object>::setTheSize(0);
 		}
 		void push(Object i){
-			if(simpleList<Object>::theSize==0){
+			if(simpleList<Object>::getTheSize()==0){
 				simpleList<Object>::create(i);
-				simpleList<Object>::theSize=1;
+				simpleList<Object>::setTheSize(1);
 			}
 			else{
 				simpleList<Object>::pushAtStart(i);
 			}
 		}
 		Object pop(){
+			Object returnValue;
+			if(simpleList<Object>::getTheSize()>0){
+				//returnValue=simpleList<Object>::head->dataField;
+				returnValue=simpleList<Object>::getHeadData();
+			}
 			simpleList<Object>::removeFromStart();
+			return returnValue;
 		}
 };
 
@@ -105,12 +112,12 @@ class queue: public simpleList<Object>
 {
 	public:
 		queue(){
-			simpleList<Object>::theSize=0;
+			simpleList<Object>::setTheSize(0);
 		}
 		void push(Object i){
-			if(simpleList<Object>::theSize==0){
+			if(simpleList<Object>::getTheSize()==0){
 				simpleList<Object>::create(i);
-				simpleList<Object>::theSize=1;
+				simpleList<Object>::setTheSize(1);
 			}
 			else{
 				simpleList<Object>::pushAtEnd(i);
@@ -118,8 +125,9 @@ class queue: public simpleList<Object>
 		}
 		Object pop(){
 			Object returnValue;
-			if(simpleList<Object>::theSize>0){
-				returnValue=simpleList<Object>::head->dataField;
+			if(simpleList<Object>::getTheSize()>0){
+				//returnValue=simpleList<Object>::head->dataField;
+				returnValue=simpleList<Object>::getHeadData();
 			}
 			simpleList<Object>::removeFromStart();
 			return returnValue;
@@ -127,16 +135,31 @@ class queue: public simpleList<Object>
 };
 
 template <typename Object>
-void checkAndPush(queue<string> *lastTwoCommand, list<simpleList<Object>* > *toEachList){
-	string popValue2=lastTwoCommand->pop();
-	string popValue3=lastTwoCommand->pop();
+void checkAndPush(string listName, string listValue, list<simpleList<Object>* > *toEachList){
 	typename list<simpleList<Object>* >::iterator it;
 	for(it=toEachList->begin();it!=toEachList->end();it++){
-		if(popValue2.compare(it->listName)==0){
-			stringstream converting(popValue3);
+		if(listName.compare((*it)->getName())==0){
+			stringstream converting(listValue);
 			Object x;
 			converting>>x;
-			it->push(x);
+			(*it)->push(x);
+		}
+	}
+}
+
+template <typename Object>
+void checkAndPop(string listName, list<simpleList<Object>* > *toEachList){
+	typename list<simpleList<Object>* >::iterator it;
+	for(it=toEachList->begin();it!=toEachList->end();it++){
+		if(listName.compare((*it)->getName())==0){
+			if((*it)->getTheSize()>0){
+				Object popReturn;
+				popReturn=(*it)->pop();
+				cout<<"Value popped: "<<popReturn<<endl;
+			}
+			else{
+				cout<<"ERROR: This list is empty!"<<endl;
+			}
 		}
 	}
 }
@@ -156,23 +179,23 @@ void createNewList(string listName, string listType, list<simpleList<Object>* > 
 }	
 
 std::string linepointer;
+std::string outFile;
 
 int checkName(queue<string> *nameLi,string theName){
 	int nameListSize=nameLi->getTheSize();
 	string checkName;
 	while(nameListSize>0){
 		checkName=nameLi->pop();
-		//cerr<<"Comparing Name: "<<checkName<<endl;
 		if((theName.compare(checkName)==0)){
 			nameLi->push(checkName);
-			return 1;		
+			return 0;		
 		}
 		else{
 			nameListSize=nameListSize-1;
 			nameLi->push(checkName);
 		}
 	}
-	return 0;				
+	return 1;				
 }
 
 int main(int argc,char* argv[]){
@@ -180,14 +203,21 @@ int main(int argc,char* argv[]){
 	getline(std::cin,linepointer);
 	ifstream commandFile;
 	const char *inputFile=linepointer.c_str();
+	commandFile.open(inputFile,ios::in);
+	cerr<<"Enter name of output file: ";
+	getline(std::cin,outFile);
+	ofstream outputResult(outFile.c_str());
+	streambuf *coutbuf=cout.rdbuf();		//Save the old cout stream
+	cout.rdbuf(outputResult.rdbuf());
+	//const char *outputFile=outFile.c_str();
+	//outputResult.open(outputFile,ios::out);
 	char *token=NULL;
 	queue<string> nameList=queue<string>();		//Create a queue to hold the name of all current queue and stack
 	list<simpleList<int> *> listSLi;
 	list<simpleList<double> *> listSLd;
 	list<simpleList<string> *> listSLs;
-	commandFile.open(inputFile,ios::in);
 	while(getline(commandFile,linepointer)){
-		cerr<<linepointer<<endl;
+		cout<<"PROCESSING COMMAND: "<<linepointer<<endl;
 		char* wholeCommand=strdup(linepointer.c_str());
 		queue<string> lineCommand=queue<string>();
 		token=strtok(wholeCommand," ");
@@ -196,30 +226,64 @@ int main(int argc,char* argv[]){
 			token=strtok(NULL," ");
 		}
 		string popValue1=lineCommand.pop();
+		string popValue2=lineCommand.pop();
+		string type=popValue2.substr(0,1);
 		if(popValue1.compare("create")==0){
-			string popValue2=lineCommand.pop();
-			if(checkName(&nameList,popValue2)==0){
+			if(checkName(&nameList,popValue2)==1){
 				nameList.push(popValue2);
-				string type=popValue2.substr(0,1);
 				string popValue3=lineCommand.pop();
 				if(type.compare("i")==0){
 					createNewList(popValue2,popValue3,&listSLi);	
 				}
+				else if(type.compare("d")==0){
+					createNewList(popValue2,popValue3,&listSLd);
+				}
+				else if(type.compare("s")==0){
+					createNewList(popValue2,popValue3,&listSLs);
+				}
+				else{
+					cout<<"ERROR: Invalid type"<<endl;
+				}
 			}
 			else{
-				cerr<<"ERROR: This name already exists!"<<endl;
+				cout<<"ERROR: This name already exists!"<<endl;
 			}	
 		}
 		if(popValue1.compare("push")==0){
-			checkAndPush(&lineCommand,&listSLi);
+			if(checkName(&nameList,popValue2)==0){
+				string popValue3=lineCommand.pop();
+				if(type.compare("i")==0){
+					checkAndPush(popValue2,popValue3,&listSLi);
+				}
+				else if(type.compare("d")==0){
+					checkAndPush(popValue2,popValue3,&listSLd);
+				}
+				else if(type.compare("s")==0){
+					checkAndPush(popValue2,popValue3,&listSLs);
+				}
+			}
+			else{
+				cout<<"ERROR: This name does not exist!"<<endl;
+			}
 		}
 		if(popValue1.compare("pop")==0){
-			string popValue2=lineCommand.pop();
-			if(checkName(&nameList,popValue2)==1){
-				
+			if(checkName(&nameList,popValue2)==0){
+				if(type.compare("i")==0){
+					checkAndPop(popValue2,&listSLi);
+				}
+				else if(type.compare("d")==0){
+					checkAndPop(popValue2,&listSLd);
+				}
+				else if(type.compare("s")==0){
+					checkAndPop(popValue2,&listSLs);
+				}
+			}
+			else{
+				cout<<"ERROR: This name does not exist!"<<endl;
 			}
 		}
 		free(wholeCommand);	
 	}
+	cout.rdbuf(coutbuf);		//Restore to the old cout stream
 	return EXIT_SUCCESS;
 }
