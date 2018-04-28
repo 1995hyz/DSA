074dbbd9e7a50d4a2f0affe3188b4bc606752b72
1995hyz <1995hyz@gmail.com>
2018-04-28 15:44:01 -0400

    Final Commit

--- sortArg.cpp
+++ sortArg.cpp
@@ -124,22 +124,31 @@ int main() {
 // You may add global variables, functions, and/or
 // class defintions here if you wish.
 #include <iterator>
-void mergeSort1(list<Data *> &l, int field);
+#include <cstring>
+void insertSort1(list<Data *> &l);
 void radixSort1(list<Data *> &l);
-void countSort1(list<Data *> &l, int exp);
+void radixSort2(list<Data *> &l);
+void radixSort3(list<Data *> &l);
+void fillArray(list<Data *> &l,Data** filledArray,int arraySize);
+void swap(Data** d1,Data** d2);
 
 void sortDataList(list<Data *> &l, int field) {
   // Fill in this function
 	if(field==1){
-		mergeSort1(l,field);
+		insertSort1(l);
 	}
-	else if(field ==2){
+	else if(field==2){
 		radixSort1(l);
+	}
+	else if(field==3){
+		radixSort2(l);
+	}
+	else if(field==4){
+		radixSort3(l);
 	}	
-	cout<<"*****"<<endl;	
 }
 
-void mergeSort1(list<Data *> &l, int field){
+void insertSort1(list<Data *> &l){
 	list<Data *>::iterator it1=l.begin();
 	list<Data *>::iterator it2=l.begin();
 	it1=next(it1,1);
@@ -165,7 +174,7 @@ void mergeSort1(list<Data *> &l, int field){
 		}
 	}
 	it1=next(l.begin(),10);
-	while(it1!=l.end()){		//The last element may be skipped?
+	while(it1!=l.end()){		
 		it2=prev(it1,1);
 		for(int i=0;i<10;i++){
 			if((*it1)->val1>=(*it2)->val1){
@@ -188,20 +197,10 @@ void mergeSort1(list<Data *> &l, int field){
 
 void radixSort1(list<Data *> &l){
 	int shift=0;
-	countSort1(l,shift);
-}
-
-void countSort1(list<Data *> &l,int shift){
 	unsigned int listSize=l.size();
 	Data** dataArray=new Data*[listSize];
-	Data** tempArray=new Data*[listSize]; 
-	for(unsigned int i=0;i<listSize;i++){
-		dataArray[i]=l.front();
-		l.pop_front();
-		if(l.empty()){
-			break;
-		}
-	}
+	Data** tempArray=new Data*[listSize];
+	fillArray(l,dataArray,listSize);
 	for(shift;shift<32;shift+=16){
 		unsigned int count[65536]={};
 		for(unsigned int i=0;i<listSize;i++){
@@ -226,4 +225,108 @@ void countSort1(list<Data *> &l,int shift){
 	for(unsigned int i=0;i<listSize;i++){
 		l.push_back(dataArray[i]);
 	}
+}
+
+void radixSort2(list<Data *> &l){
+	unsigned int listSize=l.size();
+	Data** dataArray=new Data*[listSize];
+	Data** tempArray=new Data*[listSize];
+	fillArray(l,dataArray,listSize);
+	unsigned int count[94]={};
+	for(unsigned int i=0;i<listSize;i++){
+		count[int(dataArray[i]->val3)-33]++;
+	}
+	unsigned int accArray[94]={};
+	accArray[0]=count[0];
+	for(int i=1;i<94;i++){
+		accArray[i]=accArray[i-1]+count[i];
+	}
+	for(unsigned int i=listSize-1;i>=0;i--){
+		tempArray[accArray[int(dataArray[i]->val3)-33]-1]=dataArray[i];
+		int test=accArray[int(dataArray[i]->val3)-33]--;
+		if(i==0){
+			break;
+		}
+	}
+	for(unsigned int i=0;i<listSize;i++){
+		dataArray[i]=tempArray[i];
+	}
+	for(unsigned int i=0;i<listSize;i++){
+		l.push_back(dataArray[i]);
+	}
+}
+
+unsigned int countHuge[6118750];
+unsigned int accArrayHuge[6118750];
+void radixSort3(list<Data *> &l){
+	unsigned int listSize=l.size();
+	Data** dataArray=new Data*[listSize];
+	Data** tempArray=new Data*[listSize];
+	fillArray(l,dataArray,listSize);
+	for(int shift=0;shift>=0;shift-=3){
+		for(unsigned int i=0;i<listSize;i++){
+			char tempStr[4]={};
+			strcpy(tempStr,((dataArray[i]->val4.substr(shift,3)).c_str()));
+			unsigned int* pointer=reinterpret_cast<unsigned int*>(tempStr);
+			countHuge[((__builtin_bswap32(*pointer))>>8)-0x212121]++;
+		}
+		accArrayHuge[0]=countHuge[0];
+		for(int i=1;i<6118750;i++){
+			accArrayHuge[i]=accArrayHuge[i-1]+countHuge[i];
+		}
+		for(unsigned int i=listSize-1;i>=0;i--){
+			char tempStr[4]={};
+			strcpy(tempStr,(dataArray[i]->val4.substr(shift,3)).c_str());
+			unsigned int* pointer=reinterpret_cast<unsigned int*>(tempStr);
+			tempArray[accArrayHuge[((__builtin_bswap32(*pointer))>>8)-0x212121]-1]=dataArray[i];
+			int test=accArrayHuge[((__builtin_bswap32(*pointer))>>8)-0x212121]--;
+			if(i==0){
+				break;
+			}
+		}
+		for(unsigned int i=0;i<listSize;i++){
+			dataArray[i]=tempArray[i];
+		}
+		for(unsigned int i=0;i<6118750;i++){
+			accArrayHuge[i]=0;
+		}
+	}
+	accArrayHuge[0]=countHuge[0];
+	for(int i;i<6118750;i++){
+		accArrayHuge[i]=accArrayHuge[i-1]+countHuge[i];
+	}
+	for(int i=0;i<6118750;i++){
+		int j=countHuge[i];
+		if(j==1){
+			continue;
+		}
+		else{
+			int range=accArrayHuge[i];
+			for(int k=0;k<j-1;k++){
+				for(int n=0;n<j-k-1;n++){
+					if((dataArray[range-j+n]->val4).compare(0,25,dataArray[range-j+n+1]->val4)>0){
+						swap(&dataArray[range-j+n],&dataArray[range-j+n+1]);
+					}
+				}
+			}
+		}
+	}
+	for(unsigned int i=0;i<listSize;i++){
+		l.push_back(dataArray[i]);
+	}
+	delete dataArray;
+	delete tempArray;
+}
+
+void fillArray(list<Data *> &l, Data** filledArray,int arraySize){
+	for(unsigned int i=0;i<arraySize;i++){
+		filledArray[i]=l.front();
+		l.pop_front();
+	}
+}
+
+void swap(Data** d1,Data** d2){
+	Data* temp=*d1;
+	*d1=*d2;
+	*d2=temp;
 }
