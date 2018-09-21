b85f7635588e279cdf160a41f08242d79e31d6f6
Yingzhi Hao <hao2@ice05.ee.cooper.edu>
2018-09-21 11:02:40 -0400

    Debugging

--- DSA2/spellcheck.cpp
+++ DSA2/spellcheck.cpp
@@ -8,6 +8,16 @@
 
 using namespace std;
 
+int checkWord(const string &word, hashTable *dictionary){
+	cout<<dictionary->contains(word)<<endl;
+	if(dictionary->contains(word)){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
 void loadDict(const string &dictName, hashTable* dictionary){
 	ifstream input(dictName.c_str());
 	if(!input){
@@ -35,21 +45,48 @@ void loadFile(const string &fileName, hashTable *dictionary){
 		exit(1);
 	}
 	string singleLine;
+	unsigned int lineCounter = 1;
 	while(getline(input, singleLine)){
 		std::transform(singleLine.begin(), singleLine.end(), singleLine.begin(), ::tolower);
 		string temp = "";
+		int wordCounter = 0;
+		bool containNumber = false;
 		for(int i=0; i<singleLine.length(); i++){
-			if(48<singleLine[i],
+			containNumber = (47<singleLine[i] && singleLine[i]<58);
+			if(singleLine[i]==39 || singleLine[i]==45 || containNumber || (96<singleLine[i] && singleLine[i]<123)){
+				temp = temp + singleLine[i];
+				wordCounter++;
+			}
+			else{
+				if(!temp.empty()){
+					if(wordCounter > 20){
+						cout<<"Long word at line "<<lineCounter<<", starts: "<<temp.substr(0,20)<<endl;
+					}
+					else{	
+						if(!containNumber && !checkWord(temp, dictionary)){
+							cout<<"Unknown word at line "<<lineCounter<<": "<<temp<<endl;
+						}
+					}
+				}
+				temp = "";
+				wordCounter = 0;
+			}
+		}
+		if(!temp.empty()){
+			if(wordCounter > 20){
+				cout<<"Long word at line "<<lineCounter<<", starts: "<<temp.substr(0,20)<<endl;
+			}
+			else{	
+				if(!containNumber && !checkWord(temp, dictionary)){
+					cout<<checkWord(temp, dictionary)<<endl;
+					cout<<"Unknown word at line "<<lineCounter<<": "<<temp<<endl;
+				}
+			}
+			temp = "";
+		}
+		lineCounter++;
 	}
-}
-
-int checkWord(const string &word, hashTable *dictionary){
-	if(dictionary->contains(word)){
-		return 1;
-	}
-	else{
-		return 0;
-	}
+	cout<<"File end."<<endl;
 }
 
 int main(){
@@ -68,5 +105,5 @@ int main(){
 	string outputName;
 	cout<<"Enter name of output file: ";
 	cin>>outputName;
-	
+	loadFile(inputName, &dictionary);
 }
