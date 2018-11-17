1103fe6c17d2b3563601499f2937dafad23637bd
Yingzhi Hao <hao2@ice05.ee.cooper.edu>
2018-11-16 19:34:15 -0500

    Clean

--- DSA2/Project_2/useDijkstra.cpp
+++ DSA2/Project_2/useDijkstra.cpp
@@ -1,31 +1,70 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "dijkstra.h"
+#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+//How come we never have a split function in std...
+vector<string> split(string &line, const string &delimiter){
+	int n = line.length();
+	char lineChar[n+1];
+	vector<string> arguments;
+	strcpy(lineChar, line.c_str());
+	string source = strtok(lineChar, " ");
+	string des = strtok(NULL, " ");
+	string cost = strtok(NULL, " ");
+	arguments.push_back(source);
+	arguments.push_back(des);
+	arguments.push_back(cost);
+	return arguments;
+}
+
+int generateGraph(string &filename, graph* myGraph){
+	ifstream input(filename.c_str());
+	if(!input){
+		cerr<<"Error: Cannot Open file "<<filename<<endl;
+		return 1;
+	}
+	string singleLine;
+	while(getline(input, singleLine)){
+		vector<string> arguments = split(singleLine, " ");
+		const string cost = arguments[2];
+		myGraph->addEdge(arguments[0], arguments[1], atoi(arguments[2].c_str()));
+	}
+	return 0;
+}
+
 int main(){
-	int size = 3;
-	graph testGraph = graph(size);
-	testGraph.addEdge("v1", "v2", 2);
-	testGraph.addEdge("v1", "v4", 1);
-	testGraph.addEdge("v2", "v4", 3);
-	testGraph.addEdge("v2", "v5", 10);
-	testGraph.addEdge("v3", "v1", 4);
-	testGraph.addEdge("v3", "v6", 5);
-	testGraph.addEdge("v4", "v3", 2);
-	testGraph.addEdge("v4", "v5", 2);
-	testGraph.addEdge("v4", "v6", 8);
-	testGraph.addEdge("v4", "v7", 4);
-	testGraph.addEdge("v5", "v7", 6);
-	testGraph.addEdge("v7", "v6", 1);
-	/*graph::iterator itr = testGraph.begin();
-	for(; itr != testGraph.end(); itr++){
-		cout<<(*itr).id<<endl;
-		for(int i=(*itr).adjacentEdge.size(); i>0; i--){
-			cout<<(*itr).adjacentEdge.front().destination<<endl;
-			(*itr).adjacentEdge.pop_front();
-		}
-	}*/
-	testGraph.searchPath("v1");
+	int size = 10;
+	string inputFile;
+	string outputFile;
+	cout<<"Enter name of graph file: ";
+	cin>>inputFile;
+	cout<<"Enter name of output file: ";
+	cin>>outputFile;
+	ofstream output(outputFile.c_str());
+	if(!output){
+		cerr<<"Error: Cannot open file "<<outputFile<<endl;
+		exit(1);
+	}
+	graph *dikstraGraph = new graph(size);
+	generateGraph(inputFile, dikstraGraph);
+	string id;
+	cout<<"Enter a valid vertex id for the starting vertex: ";
+	cin>>id;
+	streambuf* oldCout = cout.rdbuf();
+	streambuf* newCout = output.rdbuf();
+	cout.rdbuf(newCout);
+	clock_t t1 = clock();
+	dikstraGraph->searchPath(id);
+	clock_t t2 = clock();
+	cout.rdbuf(oldCout);
+	output.close();
+	double timeDiff = ((double)(t2 - t1) / CLOCKS_PER_SEC);
+	cout<<"Total time (in seconds) to apply Dijkstra's algorithm: "<<timeDiff<<endl;
 }
